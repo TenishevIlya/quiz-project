dc673ccd3139ead31c3dc6564a8ecf09d03eab4e
Ilya <tenishev22099@gmail.com>
2019-10-03 16:12:43 +0300

    added public and fixed admin

---
+++ admin/script.js
@@ -0,0 +1,6 @@
+function loading(){	
+  document.getElementById("spinner").classList.add('show');
+  setTimeout('location.replace("index.html")', 3000);	
+}
+let btn = document.getElementById("sign");
+btn.addEventListener('click', loading); 
