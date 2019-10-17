19789f78d4d87de2d1ad7237031e0c52a1176dbe
Alexey Uzlov <auzlov@codemastersintl.com>
2019-10-17 10:31:19 +0300

    * added issues

--- assets/scripts/Jasmine/test/app.spec.js
+++ assets/scripts/Jasmine/test/app.spec.js
@@ -1,6 +1,9 @@
 describe("App", function () {
+    // почему array объявлен здесь и почему он без var?
     array = [1,2,3,4,5];
 
+    // на один expect должен быть один it, т.е. здесь 4 теста, это правило для юнит тестов
+    // соответственно у тебя для этой домашки должно быть 4 отдельных файла, по файлу на каждый describe
     it("should say yes/no etc.", function() {
     	expect(yesOrNo(true)).toBe("yes");
     	expect(yesOrNo(false)).toBe("no");
@@ -8,12 +11,21 @@ describe("App", function () {
     	expect(yesOrNo(undefined)).toBe(null);
     });
 
+    // плохое описание, не понятно по описанию теста что тестируется именно counter
+    // за этим и пишут отдельные describe
     it("should return total or more than total", function(){
         expect(counter(4)).toEqual(4);
         expect(counter(9)).toBe(9);
         expect(counter(141)).toEqual("9+");
         expect(counter(null)).toBe(null);
         expect(counter(undefined)).toBe(null);   
+    });
+
+    // тот самый случай :(
+    describe('counter', function () {
+        it("should be correct for 0", function(){
+            expect(counter(0)).toBe(0);
+        });
     });
 
     it("should return age", function(){
@@ -23,8 +35,17 @@ describe("App", function () {
         expect(calculateAge(undefined)).toBe(null);
     });
 
+    describe('calculateAge', function () {
+        it("should be null for 12.01.2039", function(){
+            expect(calculateAge(new Date(2039, 1, 12))).toBeNull(); // -20 ?!?!
+        });
+    });
+
     it("should return random item",function() {
+        // плохо, лучше проверить например getRandomItem([1,2,3]) expect(item >= 1 && item <= 3).toBeTruthy();
         expect(array.indexOf(getRandomItem(array))).not.toBe(-1);
+
+        // плохие тесты, не относятся к функции
         expect(array).not.toBe(null);
         expect(array).not.toBe(undefined);  
     });
